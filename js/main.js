479ac81651a489cb28960deeb2e01371828d3d84
SEBASTIAN CONTENTE <scontente@kiusys.com>
2022-05-20 12:57:47 -0300

    primera

---
+++ js/main.js
@@ -0,0 +1,242 @@
+const templates={
+    home:`
+    <div id="contenedor">             
+        <div id="logo-contenedor">
+           
+                <img src="assets/images/sebas.jpg"/>
+        </div>
+      
+        <div id="contenedor">             
+            <div class="item2" >
+                <h2 align="center">Quien soy?</h2>
+                <p align="left">Me llamo Sebastián Contente, si Contente, imaginate la presión: vas a ver a un comediante, y se llama Contente. 
+                Llevo apenas 3 años con esta pasión que es el standup.
+                <br>Te invito a que me conozcas, por eso en este sitio vas a poder ver cuando actúo, donde actuo, y hasta como actúo. 
+                <br>Y por si te gusta lo que hago. te vas a poder contactar conmigo</p><br><br>                   </div>
+            </div>
+            <div class="item3" >
+            </div>
+        </div>
+    </div>
+        `,
+    contact:`
+        <div id="contact" class="container active">
+            <h2>Contacto</h2>
+            <p>Envíame tus sugerenciasde recetas a mi correo: <a :href=  ' "mailto:"+mail'   >{{mail}}</a></p>       
+        </div>`,
+    galeria:`
+    <div id="contenedor">             
+            <div class="item2" >
+            <br>
+
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/bzoJHsybxn8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+        <div class="item3" >
+        </div>
+        <div class="item3" >
+        </div>
+    </div>`,
+    escribime:`
+            <div class="contact_form">
+                <div class="formulario">  
+                    <h3 align="center">Escribime!!!</h3>
+                    <form action="show_data.html" target="_blank" >
+
+                    <label for="nombre">Nombre: <span class="obligatorio">*</span></label>
+                    <input type="text" id="nombre" name="firstname" placeholder="Your name.."  ><br>
+                  
+                    <label for="apellido">Apellido:</label>
+                    <input type="text" id="apellido" name="lastname"  placeholder="Your lastname.."><br>
+            
+                    <label for="email">E-mail:</label>
+                    <input type="email"  name="email" ><br>
+            
+                    <label for="cel">Celular:</label>
+                    <input type="text"  name="cel" ><br>
+            
+                    <label>Comentarios:</label>
+                    <textarea name="comentario" rows="" ></textarea>
+                    
+                    <input type="submit" class="submit" value="Enviar" >
+                    <br>
+                    <br>
+                    </form> 
+                </div>
+            </div>
+            `,
+        prueba:`
+        <section class="main">
+            <div id="contenedor">             
+                <div class="content">
+                    <br>
+                    <h1>Proximos shows</h1>
+                    <div class="grilla">
+                        <div class="tarjeta"  v-for="elemento in gatos"> 
+                            <img v-bind:src="'assets/images/'+elemento.imagen" v-bind:alt="elemento.alt"/>
+                            <h4>{{elemento.titulo}}</h4>
+                            <p>{{elemento.texto}}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    </section>
+    `
+}
+
+var app=new Vue({
+    el:"#app",
+    data: {
+        view : 'home',
+    },
+    components: {
+        home: {
+            data:function () {      
+                return { recetas:[  {nombre:"Wrap Doña Inés",
+                    ingredientes:"Jamón crudo, queso brie, rúcula, tomates confitados.",
+                    preparacion:" Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                    foto:"https://naturally-fresh.web.app/images/wraps/1.jpg"        
+                    },
+                    {nombre:"Wrap vegetariano",
+                    ingredientes:"Jamón crudo, queso brie, rúcula, tomates confitados.",
+                    preparacion:" Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                    foto:"https://naturally-fresh.web.app/images/wraps/2.jpg"                    
+                    }
+                ],           
+            }},
+
+            props: [],  //parametros
+            template: templates.home
+        },
+        about: {
+            data: function () {return {
+                imagen:"https://gourmetdemexico.com.mx/wp-content/uploads/2018/03/descarga.jpg",
+            }},
+            props: [],
+            template: templates.about
+        },
+        contact: {
+            data: function () {return {
+                mail:"oscarramos@gmail.com",
+            }},
+
+            
+            props: [],
+            template: templates.contact
+        } ,
+        galeria: {
+            data:function () {return {
+                
+                locales:[
+                    {nombre:"Sucursal once",
+                    direccion:"Rivadavia 150 - CABA",
+                    link:"https://youtu.be/bzoJHsybxn8",
+                    iframe: "https://youtu.be/bzoJHsybxn8"   
+                    },
+                    {nombre:"Sucursal Colegiales",
+                    direccion:"Federico Lacroze 2702 - CABA",
+                    link:"https://youtu.be/bzoJHsybxn8",
+                    iframe: "https://youtu.be/bzoJHsybxn8"   
+                    }   ,
+                    {nombre:"Sucursal Chacarita",
+                    direccion:"Federico Lacroze 3800 - CABA",
+                    link:"https://youtu.be/bzoJHsybxn8",
+                    iframe: "https://youtu.be/bzoJHsybxn8"   
+                    }
+                ]
+            }},
+            props: [],
+            template: templates.galeria
+        } ,
+        escribime: {
+            props: [],
+            template: templates.escribime
+        },
+        delivery: {
+            data:function () {return {
+                
+                bebidas:[
+                    {
+                        nombre:"Sprite",
+                        precio: "120",
+                        categoria: "gaseosa",
+                        imagen:"https://st.depositphotos.com/2121815/4748/i/950/depositphotos_47482769-stock-photo-bottle-of-sprite-drink-isolated.jpg"
+                    },
+                    {
+                        nombre:"coca cola",
+                        precio:"125",
+                        categoria:"gaseosa",
+                        imagen:"https://st.depositphotos.com/1010050/4422/i/950/depositphotos_44227185-stock-photo-coca-cola-bottle.jpg"
+                    }
+                ]
+
+
+
+            }},
+            props: [],
+            template: templates.delivery
+        },
+        prueba2: {
+            props: [],
+            template: templates.prueba2
+        },
+        prueba: {
+            data:function () {return {
+                
+                gatos:[
+                    {
+                        imagen:"show1.jpeg",
+                        alt:"Gatitos Duermen",
+                        titulo: "Domingo 26 de Junio de 2022",
+                        texto:"Brako Bar, Alem 283, Ramos Mejia"
+                    },
+                    {
+                        imagen:"show2.jpeg",
+                        alt: "Gatitos Lindos",
+                        titulo: "Miercoles 18 de Mayo de 2022",
+                        texto:"Vale Reir, Casa Jache"
+                    },
+                    {
+                        imagen:"show3.jpeg",
+                        alt: "Viernes 24 de Junio de 2022",
+                        titulo: "Kaputt",
+                        texto:"Teatro el Cubo"
+                    },
+                    {
+                        imagen:"",
+                        alt: "",
+                        titulo: "",
+                        texto:""
+                    },
+                    {
+                        imagen:"",
+                        alt: "",
+                        titulo: "",
+                        texto:""
+                    },
+                    {
+                        imagen:"",
+                        alt: "",
+                        titulo: "",
+                        texto:""
+                    },
+                    {
+                        imagen:"",
+                        alt: "",
+                        titulo: "",
+                        texto:""
+                    }
+                ]
+                     }},
+            props: [],methods: {
+                Alertar: function (message, event) {
+                  // ahora tenemos acceso al evento nativo.
+                  if (event) event.preventDefault()
+                  alert(message)
+                }
+              },
+            template: templates.prueba
+        }
+    }
+    
+}
+)
