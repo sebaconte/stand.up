9c9c1a0e951c3ea32aed313be5954ba01e1b1f5e
sebaconte <sebaconte@gmail.com>
2022-05-24 10:37:37 -0300

    trece

--- js/main.js
+++ js/main.js
@@ -322,7 +322,7 @@ var app=new Vue({
 }
 )
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function myFunction() {
+function hamburguesa() {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
       x.className += " responsive";
